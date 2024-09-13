39a4edce8414342b621c904b3eb738ef6090fab6
Mustafa <pourmhere@gmail.com>
2024-09-13 21:23:27 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,19 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+import "./../styles/App.css";
 
+const App = () => {
+  const [str, setStr] = useState("");
+  // let str = "";
+  function handleClick(e) {
+    setStr("Hello " + e.target.value + "!");
+    console.log(str);
+  }
+  return (
+    <div id="main">
+      {/* Do not remove the main div */}
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  );
+};
+
+export default App;
